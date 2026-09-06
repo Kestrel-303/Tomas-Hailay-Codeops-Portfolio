29fb343d71275f7965891b39f8bf9b2575faa49a
Kestrel-303 <tomashailaygidey@gmail.com>
2026-09-06 21:32:37 +0300

    submission: Day#5 mini project
    include
    a custom hook to fetch data
    a cart reducer file to handle logic like add and remove
    a cartPrvider component that handles cart logic and dispatch
    category filter using fetch

---
+++ module-03-react-nextjs/mini-projects/mini-project-5/addis-eatery/src/components/CartProvider.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react-refresh/only-export-components */
+import { createContext, useContext, useReducer, useMemo } from "react";
+import { cartReducer } from "./cartReducer";
+
+export const CartContext = createContext(null);
+
+export function CartProvider({ children }) {
+  const [state, dispatch] = useReducer(cartReducer, { items: [] });
+  const total = state.items.reduce((s, d) => s + d.price, 0);
+
+  const value = useMemo(
+    () => ({ items: state.items, dispatch, total }),
+    [state.items, total]
+  );
+
+  return (
+    <CartContext.Provider value={value}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart() {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error("useCart must be used within a CartProvider");
+  }
+  return context;
+}
+
+export default CartProvider;
